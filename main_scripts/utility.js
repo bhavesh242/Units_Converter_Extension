715c93334a70adf510d5a8e32a2e4137965990c2
Durga71 <47115497+Durga71@users.noreply.github.com>
2020-09-13 21:17:07 -0400

    Update utility.js

--- main_scripts/utility.js
+++ main_scripts/utility.js
@@ -26,6 +26,9 @@ function get_conversion_class(type, unit) {
     if (type == "temperature") {
         return new Temperature(unit, get_possible_conversions_list(type, unit));
     }
+    else if (type == "length") {
+        return new Length(unit, get_possible_conversions_list(type, unit));
+    }
 }
 
 //Get a number's precision
